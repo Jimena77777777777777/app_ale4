e575da9e225232af87197fcc3e082c8ce2588b08
Alejandra <202100355d@utea.edu.pe>
2024-06-27 07:15:37 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/app_ale4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_ale4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:app_ale4/home_page.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
